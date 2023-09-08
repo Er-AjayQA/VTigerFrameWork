0d049ba80896c4aaa6423816bf32015c948e4882
Ajay Kumar <AuthorName@AjayKumar>
2023-09-08 11:21:48 +0530

    Adding paths for "ExcelFile" and "Property File" in "IConstant" interface.

---
+++ src/main/java/com/VtigerFrameWork/GenericUtilities/IConstantUtility.java
@@ -0,0 +1,14 @@
+package com.VtigerFrameWork.GenericUtilities;
+
+/**
+ * @author Ajay Kumar
+ * This interface consist all the paths which will static for everyone.
+ * It consist "ExcelFilePath" and "PropertyFilePath".
+ */
+
+public interface IConstantUtility {
+	
+	String excelFilePath= ".\\src\\test\\resources\\TestData\\VtigerTestData.xlsx";
+	String propertyFilePath= ".\\src\\test\\resources\\ConfigFiles\\VTigerConfig.properties";
+
+}
